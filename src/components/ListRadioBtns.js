50bc351bf99efb45d234d1cbb631e272586b58b2
Kheoh Yee Wei <kwei88@gmail.com>
2019-04-05 10:37:28 +0800

    refactor(app): extract <ErrorMessage />

--- src/components/ListRadioBtns.js
+++ src/components/ListRadioBtns.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { css } from "@emotion/core";
-import { ErrorMessage } from "formik";
 import { List } from "./List";
 import RadioButton from "./RadioButton";
 import spacing from "../css/spacing";
-import ErrorMsg from "./ErrorMsg";
 
 function ListRadioBtns({
   field: { name, value, onChange },
@@ -12,38 +10,24 @@ function ListRadioBtns({
   ...props
 }) {
   return (
-    <div
-      style={{
-        position: "relative"
-      }}
-    >
-      <List>
-        {options.map(option => (
-          <li key={id}>
-            <RadioButton
-              className={css`
-                padding: ${spacing.space2};
-              `}
-              handleChange={onChange}
-              name={name}
-              value={option}
-              isChecked={option === value}
-              {...props}
-            >
-              {option}
-            </RadioButton>
-          </li>
-        ))}
-      </List>
-      <ErrorMessage
-        name={name}
-        component={ErrorMsg}
-        css={css`
-          left: ${spacing.space0};
-        `}
-        htmlFor="questionnaire-forms"
-      />
-    </div>
+    <List>
+      {options.map(option => (
+        <li key={id}>
+          <RadioButton
+            className={css`
+              padding: ${spacing.space2};
+            `}
+            handleChange={onChange}
+            name={name}
+            value={option}
+            isChecked={option === value}
+            {...props}
+          >
+            {option}
+          </RadioButton>
+        </li>
+      ))}
+    </List>
   );
 }
 
