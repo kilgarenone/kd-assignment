a5baa686326946f8d5df5a0d78bd870d06f2dc97
Kheoh Yee Wei <kwei88@gmail.com>
2019-04-05 10:37:45 +0800

    feat(app): bind textarea to formik

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { css } from "@emotion/core";
 
-function TextArea() {
+function TextArea({ field: { name, value, onChange } }) {
   return (
     <textarea
+      name={name}
+      value={value}
+      onChange={onChange}
       css={css`
         width: 100%;
         height: 100%;
