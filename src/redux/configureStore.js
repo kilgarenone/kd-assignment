b47077abdbb3f4db14dc5a148935356e140271c7
Kheoh Yee Wei <kwei88@gmail.com>
2019-04-06 13:48:30 +0800

    fixes(app): removes redux-thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,17 +1,10 @@
 import { applyMiddleware, createStore } from "redux";
-import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import rootReducer from "./reducers";
 
 export default function configureStore(preloadedState = undefined) {
-  const middlewares = [thunkMiddleware];
-  const middlewareEnhancer = applyMiddleware(...middlewares);
-
-  const enhancers = [middlewareEnhancer];
-  const composedEnhancers = composeWithDevTools(...enhancers);
-
-  const store = createStore(rootReducer, preloadedState, composedEnhancers);
+  const store = createStore(rootReducer, preloadedState);
 
   if (process.env.NODE_ENV !== "production" && module.hot) {
     module.hot.accept("./reducers", () => store.replaceReducer(rootReducer));
